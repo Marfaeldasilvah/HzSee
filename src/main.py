8544c1c558215c8e4d280d3a140ffad477a39c3c
rPlakama <rPlakama@proton.me>
2026-09-17 19:34:43 -0300

    WIP in OpenCV --> Yolo ponte

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
+# import math
 import sys
 
 import cv2 as cv
 
+#   import cvzone
+#   import ultra
+# from ultralytics import YOLO
+
 cap = cv.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+# model
 
 if not cap.isOpened():
     print("Cannot open camera")
@@ -13,9 +22,11 @@ while True:
     if not ret:
         print("Can't receive frame...exiting")
         break
+
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     cv.imshow("frame", gray)
     if cv.waitKey(1) == ord("q"):
         break
+
 cap.release()
 cv.destroyAllWindows()
