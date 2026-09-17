98921f5d5137e52e907ea8b1a3cbb521563dabce
rPlakama <rPlakama@proton.me>
2026-09-17 19:26:25 -0300

    Camera test

--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
+import sys
+
 import cv2 as cv
-import numpy as np
 
+cap = cv.VideoCapture(0)
 
-# mouse callback function
-def draw_circle(event, x, y, flags, param):
-    if event == cv.EVENT_LBUTTONDBLCLK:
-        cv.circle(img, (x, y), 100, (255, 0, 0), -1)
+if not cap.isOpened():
+    print("Cannot open camera")
+    sys.exit()
+while True:
+    ret, frame = cap.read()
 
-
-# Create a black image, a window and bind the function to window
-img = np.zeros((512, 512, 3), np.uint8)
-cv.namedWindow("image")
-cv.setMouseCallback("image", draw_circle)
-
-while 1:
-    cv.imshow("image", img)
-    if cv.waitKey(20) & 0xFF == 27:
+    if not ret:
+        print("Can't receive frame...exiting")
         break
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow("frame", gray)
+    if cv.waitKey(1) == ord("q"):
+        break
+cap.release()
 cv.destroyAllWindows()
